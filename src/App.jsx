81d5c9c7b91746ed1c898b10dffe9417a48b2a17
Dios <abhishekpatawari.codes@gmail.com>
2024-06-14 17:34:22 +0530

    add:functionality updated:readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,30 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 const App = () => {
+const [images,setImages] = useState([]);
+const [isLoading,setIsLoading] = useState(true);
+const [term,setTerm] = useState('');
+
+useEffect(()=>{
+  fetch(`https://pixabay.com/api/?key=${import.meta.env.VITE_REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+  .then(res =>res.json())
+  .then(data => {
+    setImages(data.hits);
+    setIsLoading(false);
+  })
+  .catch(err=>console.log(err))
+},[term])
   return (
-    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-      <img src="https://picsum.photos/200/300" alt="" className='w-full' />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by john doe
-        </div>
-        <ul>
-        <li>
-            <strong>Views: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            3000
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            400
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag3
-        </span>
-      </div>
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text)=>setTerm(text)} />
+        {!isLoading && images.length === 0 && <h1 className="text-6xl text-center mx-auto mt-32">Images not found...</h1>}
+      {isLoading? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>:<div className="grid grid-cols-3 gap-4">
+        {images.map(image=>(
+          <ImageCard key={image.id} image={image}/>
+        ))}
+      </div>}
     </div>
   )
 }
